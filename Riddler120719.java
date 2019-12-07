6d40cf5bc444be03b77b733a0b3f82d8fced39ee
brentroberts989 <brentroberts989@gmail.com>
2019-12-07 14:31:51 -0500

    Added some stuff

--- Riddler120719.java
+++ Riddler120719.java
@@ -1,3 +1,33 @@
 /**
+ * A class to solve the 12/7/2019 Riddler Classic Problem
  * 
+ * @author Brent Roberts
  */
+public class Riddler120719 {
+
+
+    /**
+     * Guess what this does?
+     * 
+     * @param args Not used
+    */
+    public static void main(String[] args) {
+        System.out
+    }
+
+    /**
+     * Calculates the average button presses from 42 of a random number
+     */
+    private static void avgRand() {
+
+    }
+
+    /**
+     * Calculates how many button presses to 42.
+     * 
+     * @param songNumber the number of the song to be tested.
+     */
+    private static int presses(int songNumber) {
+        return songNumber > 42 ? 42- songNumber : 143 - songNumber;
+    }
+}
