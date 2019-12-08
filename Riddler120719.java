5d951c88619bf7cfce58bd26a04eb9fe0adb82a7
brentroberts989 <brentroberts989@gmail.com>
2019-12-07 21:30:18 -0500

    Found average number of presses of a random

--- Riddler120719.java
+++ Riddler120719.java
@@ -12,15 +12,9 @@ public class Riddler120719 {
      * @param args Not used
     */
     public static void main(String[] args) {
-        System.out
+        System.out.println(avgRand());
     }
 
-    /**
-     * Calculates the average button presses from 42 of a random number
-     */
-    private static void avgRand() {
-
-    }
 
     /**
      * Calculates how many button presses to 42.
@@ -28,6 +22,25 @@ public class Riddler120719 {
      * @param songNumber the number of the song to be tested.
      */
     private static int presses(int songNumber) {
-        return songNumber > 42 ? 42- songNumber : 143 - songNumber;
+        return songNumber <= 42 ? 42- songNumber : 142 - songNumber;
+    }
+
+    /**
+     * Calculates the average button presses from 42 of a random number
+     * (results in 49)
+     */
+    private static int avgRand() {
+        int result = 0;
+        for (int i = 1; i <= 100; i++) {
+            result += presses(i);
+        }
+        return result/100;
+    }
+
+    /**
+     * Code to be run to calculate average number of presses
+     */
+    private static int avgPresses() {
+
     }
 }
